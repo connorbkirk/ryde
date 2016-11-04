71b68c416b27d85cd375cdce04588932c6516638
connorbkirk <ckirk96@gmail.com>
2016-11-03 21:32:43 -0400

    Add files via upload
    Added model layers. Will need to be updated when we figure out more DB stuff.

---
+++ Ryde/src/objectlayer/Car.java
@@ -0,0 +1,99 @@
+package objectlayer;
+
+
+//this class is the model of the car
+public class Car {
+	private String make;
+	/**
+	 * @return the make
+	 */
+	public String getMake() {
+		return make;
+	}
+	/**
+	 * @param make the make to set
+	 */
+	public void setMake(String make) {
+		this.make = make;
+	}
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+	/**
+	 * @return the year
+	 */
+	public String getYear() {
+		return year;
+	}
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(String year) {
+		this.year = year;
+	}
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+	/**
+	 * @return the ownerId
+	 */
+	public int getOwnerId() {
+		return ownerId;
+	}
+	/**
+	 * @param ownerId the ownerId to set
+	 */
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private String model;
+	private String year;
+	private String color;
+	private int ownerId;
+	private int price;
+	private String description;
+	
+	
+}
