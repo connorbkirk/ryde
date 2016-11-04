7dad83cb76a23be47247b3aaa01bb1cd88dd4b61
connorbkirk <ckirk96@gmail.com>
2016-11-03 23:06:27 -0400

    Add files via upload

--- Ryde/src/persistlayer/UserPersistImpl.java
+++ Ryde/src/persistlayer/UserPersistImpl.java
@@ -1,7 +1,23 @@
 package persistlayer;
 
+import java.sql.Connection;
+
 //This class is the primary persistent (model) layer class. 
 //It explicitly communicates with the DbAccessImpl
 public class UserPersistImpl {
+
+	//global variables
+	DbAccessImpl db = new DbAccessImpl();
+	Connection con = db.connect();
+	
+	public void register(String username, String password, String firstName, String lastName) {
+		//this movie adds a row into 'users' with the given username and password
+		
+		String query = "INSERT INTO users (username, pswrd, firstName, lastName) "+
+		"VALUES(\'"+username+"\', \'"+ password + "\', \'" + firstName + "\', \'" + lastName + "\')";
+		
+		db.create(con, query);
+		
+	}
 	
 }
