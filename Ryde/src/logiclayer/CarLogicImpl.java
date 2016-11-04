b11e50cca5e9854ef13f6f1163924855dbcffd23
vtec11001 <SahiComputer@host-222-68.gaanriv.athens.ga.us.clients.pavlovmedia.com>
2016-11-04 15:01:01 -0400

    LogicLayer additions
    added some basic logic layer things nothing too fancy

--- Ryde/src/logiclayer/CarLogicImpl.java
+++ Ryde/src/logiclayer/CarLogicImpl.java
@@ -1,7 +1,63 @@
-package logiclayer;
+package Ryde.logiclayer;
 
-//This class is the controller class for all the functionalities of cars.
-//It directly interacts with the Servlet and car persistent layer classes.
-public class CarLogicImpl {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Ryde.objectlayer.Car;
+import Ryde.persistlayer.RydePersistImpl;
+
+public class CarLogicImpl 
+
+{
+	private RydePersistImpl rpi; 
+	
+	
+	public CarLogicImpl() throws ClassNotFoundException
+	{
+		rpi = new RydePersistImpl(); 
+	}
+	
+	
+	public ArrayList<Car> getCars() throws ClassNotFoundException, SQLException
+	{
+		ResultSet rs = null; 
+		rs = rpi.returnCars(); 
+		
+		ArrayList<Car>resultCars = new ArrayList<Car>(); 
+		
+		
+		while(rs.next())
+		{
+			String make; 
+			String model; 
+			int carYear; 
+			String color; 
+			int price; 
+			int id; 
+			String description; 
+			
+			make = rs.getString("make"); 
+			model = rs.getString("model"); 
+			carYear = rs.getInt("carYear"); 
+			color = rs.getString("color"); 
+			price = rs.getInt("price"); 
+			id = rs.getInt("userId"); 
+			description = rs.getString("userDescription"); 
+			//retrieves info from the result set. 
+			
+			
+			resultCars.add(new Car(model, carYear, color, price, id, description)); 
+			//dynamically creates car objects. 
+			
+			
+		}
+		
+		return resultCars; 
+		
+	}
+	
+	
+	
 	
 }
