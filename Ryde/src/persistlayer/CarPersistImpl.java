41158ee47934798192e7a462f1dfcf085a1c4c9f
vtec11001 <smn73822@uga.edu>
2016-11-04 20:35:25 -0400

    Update CarPersist
    Add some other methods

--- Ryde/src/persistlayer/CarPersistImpl.java
+++ Ryde/src/persistlayer/CarPersistImpl.java
@@ -11,11 +11,47 @@ public class CarPersistImpl {
 	DbAccessImpl db = new DbAccessImpl();
 	Connection con = db.connect();
 	
-	public ResultSet getCars() {
+	public ResultSet getCars() 
+	{
 		ResultSet rs;
 		String query = "SELECT * FROM cars";
 		rs = db.retrieve(con, query);
 		return rs;
 	}
+	
+  	public int addCar(String make, String model, int carYear, String color) throws SQLException
+	    {
+		String query = "insert into cars (make, model carYear, color) values (" + "\"" + make + "\", " + "\"" + model + "\", " + carYear + ", " + "\"" + color + "\"" + ")";
+
+		return dbai.create(con, query);
+
+		//used to add car in db.
+	    }
+    
+	    public int deleteCar(String name) throws SQLException
+	    {
+		String query = "delete from cars name = " + "\"" + name + "\"";
+
+		return dbai.delete(con, query);
+		//used to delete a single car.
+	    }
+    
+	    public ResultSet returnSingleCar(String userModel) throws SQLException
+	    {
+		ResultSet rs = null;
+
+
+		String query = "select * from cars where model = " + "\"" + userModel + "\"";
+
+		rs = dbai.retrieve(con, query);
+
+		//used to return single car.
+		//used when selecting single car on user end.
+
+		return rs; 
+	    }
+	
+	
+	
 	
 }
