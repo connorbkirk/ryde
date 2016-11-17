7697e9305c1d363cdd2a140cf9468a8b843afae6
vtec11001 <smn73822@uga.edu>
2016-11-17 00:39:06 -0500

    added the verify single date

--- Ryde/src/persistlayer/RentalPersistImpl.java
+++ Ryde/src/persistlayer/RentalPersistImpl.java
@@ -25,6 +25,16 @@ import java.sql.ResultSet;
     return db.retrieve(con, query); 
   
   }
+	  
+public ResultSet verifyDate(int carID, String d1, String d2)
+{
+	
+	//used to check if date is in the DB for cetain car.If it is this means that we cannot rent for that time frame for that car. 
+	String query = "SELECT * FROM rental_dates WHERE carID = " + carID + "AND WHERE start_date=  " + d1 + " AND WHERE end_date= " + d2; 
+	
+	return db.retrieve(con, query); 
+}
+  
   
   
   }
