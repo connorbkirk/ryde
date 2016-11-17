5eadda97fbc594a5adeb746d5859798d35d55a5c
JayRPorter <JayP@s172-20-240-h124.paws.uga.edu>
2016-11-17 11:30:06 -0500

    added beginning to search.js

--- Ryde/WebContent/search.js
+++ Ryde/WebContent/search.js
@@ -1,1 +1,24 @@
 //This is the js file to implement the car preview feature of the search.
+var searchbar = $("searchbar");
+
+function showResult(str) {
+  if (str.length==0) { 
+    document.getElementById("jumbotron").innerHTML="";
+    document.getElementById("jumbotron").style.border="0px";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  //replace with ajax //xmlhttp.open("GET","livesearch.php?q="+str,true);
+  //replace with ajax //xmlhttp.send();
+}
