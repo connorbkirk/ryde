dd45c72cc7ba4ffdc1f82b7ef566a8a6ce43514f
JayRPorter <JayP@Jays-MacBook-Pro.local>
2016-11-23 23:42:17 -0500

    working on autocomplete

---
+++ Ryde/src/logiclayer/SearchLogicImpl.java
@@ -0,0 +1,52 @@
+//TODO
+package logiclayer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import javax.servlet.http.Part;
+
+import com.ibm.wsdl.util.IOUtils;
+
+import persistlayer.SearchPersistImpl;
+
+public class SearchLogicImpl{
+
+    private SearchPersistImpl sp;
+
+    public SearchLogicImpl(){
+	sp = new SearchPersistImpl();
+    }
+
+    public List<String> getSuggestionsFromPersist(String currentSearchBarString){
+	ResultSet rs = null;
+	rs = sp.getSuggestions(currentSearchBarString);
+
+	List<String>resultMakes = new ArrayList<String>();
+
+	try {
+	    while(rs.next()){
+		String make = rs.getString(1);
+   
+
+		resultMakes.add(make);
+		//dynamically creates strings for makes.                                                                                                                                          
+	    }
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block                                                                                                                                                  
+	    e.printStackTrace();
+	}
+
+	return resultCars;
+    }
+
+
+}
---
+++ Ryde/src/persistlayer/SearchPersistImpl.java
@@ -0,0 +1,24 @@
+package persistlayer;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class SearchImpl[
+    
+    //global variables
+	DbAccessImpl db = new DbAccessImpl();
+	Connection con = db.connect();
+    
+    public ResultSet getSuggestions(String searchbarContent){
+        ResultSet rs;
+		String query = "SELECT * FROM cars WHERE make LIKE "+ searchbarContent;
+		rs = db.retrieve(con, query);
+		return rs;
+    }//getSuggestions
+    
+		
+]//SearchImpl
