761d5a09ca94a540fd2aed1995bc644423fc9943
JayRPorter <JayP@Jays-MacBook-Pro.local>
2016-11-27 15:40:42 -0500

    added cars, users, rental date

--- dbSrc/ryde-populate.sql
+++ dbSrc/ryde-populate.sql
@@ -4,20 +4,44 @@
 #
 
 #Add one owner to the database
-INSERT INTO users 
+INSERT INTO users
 VALUES('1','John','Porter','jayp','password');
 
-#Add two cars to the database
-INSERT INTO cars 
+#Add cars to the database
+INSERT INTO cars
 VALUES('1','sedan','Toyota','Camry','2016','silver','1','200','nice and reliable car');
 
-INSERT INTO cars 
+INSERT INTO cars
 VALUES('2','sedan','Saturn','Ion','2004','silver', '1','75','Car has been in two accidents, completely fixed up and safe');
 
-#add rental dates for the Toyota
+INSERT INTO cars
+VALUES('3','truck','Ford','BigTruck','2024','chrome','2','200','You have not driven a car so impractical before');
+
+INSERT INTO cars
+VALUES('4','sedan','Mercedes','Mersais','2016','blue','5', '1','He will roll up in the Mersais with a check to your work place' );
+
+INSERT INTO cars
+VALUES('5','boat', 'Toyota', 'BigRaft', '2004','white','4','20','Before you say there is no such thing just trust us that it exists');
+
+INSERT INTO cars
+VALUES('6','truck','Ford','F650','2018','black','3','100','Such a big truck it could move a house');
+
+#add rental dates for the cars
 INSERT INTO rental_dates
 VALUES('1','1','2016-9-16','2016-11-15');
 
-#Add two users to the database
+INSERT INTO rental_dates
+VALUES('2','2','2016-9-22', '2016-9-23');
+
+#Add users to the database
 INSERT INTO users(id, firstname,lastname,username,pswrd)
 VALUES('2','Connor', 'Kirk','username','bologna1');
+
+INSERT INTO users
+VALUES('3', 'Mehdi', 'Allahyari', 'mehdi', 'firebug');
+
+INSERT INTO users
+VALUES('4','Sahi', 'Nimmakayalu', 'sahi', 'password');
+
+INSERT INTO users
+VALUES('5','Kanye','West','kanye','kimk');
