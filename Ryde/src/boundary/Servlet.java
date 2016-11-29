168aedfc01597bdfc805bbab72d3bfca4e04f325
Connor Kirk <ckirk96@gmail.com>
2016-11-28 21:27:30 -0500

    Added Javadoc style comments

--- Ryde/src/boundary/Servlet.java
+++ Ryde/src/boundary/Servlet.java
@@ -4,10 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -34,10 +30,13 @@ import objectlayer.Image;
 import objectlayer.User;
 
 /**
- * Servlet implementation class Servlet
+ * Servlet class.
+ * This class handles all requests and responses.
+ * It directly interacts with the controller class.
+ * 
+ * @author Connor Kirk
+ * @version 1.00, 5 Dec 2016
  */
-//this class handles all the requests and responses. 
-//It directly interacts with the controller class.
 @WebServlet("/Servlet")
 @MultipartConfig
 public class Servlet extends HttpServlet {
@@ -57,6 +56,10 @@ public class Servlet extends HttpServlet {
         super();
     }
 
+    /**
+     * Initializes global variables and configures the servlet.
+     * @see javax.servlet.GenericServlet#init()
+     */
     public void init() {
     	//intialized global vars
     	df = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25);//set object wrapper builder
@@ -89,18 +92,22 @@ public class Servlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		processRequest(request, response);
 	}
 	
+	/**
+	 * This methods processes the request of the user and redirects to the proper method.
+	 * 
+	 * @param request Takes user's request. Gathers parameters
+	 * @param response Used for redirecting the user.
+	 * @throws IOException If there is an invalid request.
+	 */
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		//This function processes the request of the user and redirects them to the proper method
 		response.setContentType("text/html");
 		out = response.getWriter();//initialize writer
 		root = new SimpleHash(df.build());//initialize root map
 		
 		//route request
-		//--> use parameter --> req
 		switch(request.getParameter("req")){
 			case "register":
 				register(request, response);
@@ -144,7 +151,13 @@ public class Servlet extends HttpServlet {
 		}
 	}
 	
-	//this is the method that handles the upload.ftl view
+	/**
+	 * This method handles the upload request and directs the user's view to the
+	 * upload.ftl template.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	private void upload(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
 		
@@ -159,6 +172,15 @@ public class Servlet extends HttpServlet {
 		processTemplate("upload.ftl");
 	}
 
+	/**
+	 * This method receives an image through the HttpServletRequest object,
+	 * calls for the logic layer to store it in the database, and prints a 
+	 * JSON object containing the image through the output stream to be 
+	 * parsed with jquery.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for writing output to be used with JQuery.
+	 */
 	private void uploadImage(HttpServletRequest request, HttpServletResponse response) {
 		int carId = Integer.parseInt(request.getParameter("id"));
 		CarLogicImpl carCtrl = new CarLogicImpl();
@@ -193,6 +215,17 @@ public class Servlet extends HttpServlet {
 		}
 	}
 
+	
+	/**
+	 * This method receives an image id through the HttpServletRequest object
+	 * and calls for the car logic layer to remove it from the database. 
+	 * If the user is not logged in, they will be redirected
+	 * to the login page. If they are not owners of the car, they will be
+	 * redirected to the last page they visited.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	private void deleteImage(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
 		CarLogicImpl carCtrl = new CarLogicImpl();
@@ -221,6 +254,16 @@ public class Servlet extends HttpServlet {
 		
 	}
 
+	/**
+	 * This method receives a car id through the HttpServletRequest object
+	 * and calls for the car logic layer to remove the item from the database
+	 * if the user has permission. If the user is not logged in, they will be 
+	 * redirected to the login page. If they are not owners of the car, they 
+	 * will be redirected to the last page they visited.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	private void delete(HttpServletRequest request, HttpServletResponse response) {
 		//create logic and session objects
 		CarLogicImpl carCtrl = new CarLogicImpl();
@@ -252,6 +295,17 @@ public class Servlet extends HttpServlet {
 		
 	}
 
+	/**
+	 * 
+	 * This method receives a car id through the HttpServletRequest object.
+	 * It allows the user to modify the car's entry in the database if they
+	 * have permission. If the user is not logged in, they will be redirected
+	 * to the login page. If they are not owners of the car, they will be
+	 * redirected to the last page they visited.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	private void edit(HttpServletRequest request, HttpServletResponse response) {
 		//create logic and session objects
 		CarLogicImpl carCtrl = new CarLogicImpl();
@@ -303,6 +357,15 @@ public class Servlet extends HttpServlet {
 		}
 	}
 
+	/**
+	 * This method receives information about a car through the
+	 * HttpServletRequest object. It allows the user to make a new 
+	 * entry in the database if they are logged in. If they are not
+	 * logged in, they will be redirected to the login page.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	private void add(HttpServletRequest request, HttpServletResponse response) {
 		CarLogicImpl carCtrl = new CarLogicImpl();
 		HttpSession session = request.getSession();
@@ -319,14 +382,11 @@ public class Servlet extends HttpServlet {
 		//if user is logged in, allow them to add 
 		if(session.getAttribute("user") != null){
 			//if all parameters are not null, insert car into db.
-			//deal with making sure params are not null with js
 			if(make!=null && model != null && year != null && color != null && price != null
 					&& description != null && carType != null){
 				int ownerId = (Integer) session.getAttribute("user");
 				int carId = carCtrl.addCar(ownerId, make, model, year, color, 
 						price, description, carType);
-				//uploadCarImages(request, carId);
-				//root.put("id", carId);
 				
 				//route user to upload images
 				try {
@@ -349,6 +409,15 @@ public class Servlet extends HttpServlet {
 		
 	}
 
+	/**
+	 * This method receives a user id through the HttpServletRequest
+	 * object and calls for the user's information to be processed by
+	 * FreeMarker with the user.ftl file. If a user is trying to view
+	 * his/her own page, additional capabilities (edit/delete) will be
+	 * shown on car listings.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 */
 	void viewUser(HttpServletRequest request){
 		//create session and logic objects
 		HttpSession session = request.getSession();
@@ -380,6 +449,14 @@ public class Servlet extends HttpServlet {
 		processTemplate("user.ftl");
 	}
 	
+	/**
+	 * This method receives a car id through the HttpServletRequest
+	 * object and calls for the car's information to be processed by
+	 * FreeMarker with the car.ftl file. If a user is trying to view
+	 * a car they own an edit button will be added to the page.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 */
 	void viewCar(HttpServletRequest request){
 		//create session and logic objects
 		CarLogicImpl carCtrl = new CarLogicImpl();
@@ -401,12 +478,19 @@ public class Servlet extends HttpServlet {
 			if(userId == ownerId){
 				root.put("same", true);
 			}
-			
 		}
 		
 		processTemplate("car.ftl");
 	}
 	
+	/**
+	 * This method receives criteria through the HttpServletRequest
+	 * object and uses it to narrow down the list of cars that will
+	 * be processed and displayed using FreeMarker in the search.ftl
+	 * file.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 */
 	void search(HttpServletRequest request) {
 		//create logic and session objects
 		CarLogicImpl carCtrl = new CarLogicImpl();
@@ -441,9 +525,18 @@ public class Servlet extends HttpServlet {
 		root.put("cars",cars);
 		
 		processTemplate("search.ftl");
-		
 	}
 	
+	
+	/**
+	 * This method receives the information of a new user from the
+	 * register.html page. If the username does not exist in the
+	 * database, a new entry is made and the user's session is stored. 
+	 * If the username does exist, the user is redirected to register.html.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	void register(HttpServletRequest request, HttpServletResponse response){
 		//create logic and session objects
 		UserLogicImpl userCtrl = new UserLogicImpl();
@@ -475,6 +568,16 @@ public class Servlet extends HttpServlet {
 		
 	}
 	
+	/**
+	 * This method verifies that the information the user inputs in
+	 * login.html is valid. If so, their user id is stored in the
+	 * HttpSession and the user is routed to the last page they were
+	 * on. If the information is invalid, the user is rerouted to
+	 * login.html.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	void login(HttpServletRequest request, HttpServletResponse response){
 		//create logic and session objects
 		UserLogicImpl userCtrl = new UserLogicImpl();
@@ -486,18 +589,25 @@ public class Servlet extends HttpServlet {
 		
 		//validate user - if credentials are valid, route to search
 		//if invalid route to login
-		try {
-			if(userCtrl.validate(username, password)){
-				session.setAttribute("user", userCtrl.getIdFromUsername(username));
-				sendToLastPage(response, session);
+		if(userCtrl.validate(username, password)){
+			session.setAttribute("user", userCtrl.getIdFromUsername(username));
+			sendToLastPage(response, session);
+		} else{
+			try {
+				response.sendRedirect("login.html");
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			else
-				response.sendRedirect("login.html");//do something with js?
-		} catch (NoSuchAlgorithmException | SQLException | IOException e) {
-			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * This method invalidates the user's session and routes
+	 * them to index.html.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @param response Used for redirecting the user.
+	 */
 	void logout(HttpServletRequest request, HttpServletResponse response){
 		HttpSession session = request.getSession();
 		session.invalidate();
@@ -509,6 +619,12 @@ public class Servlet extends HttpServlet {
 		}
 	}
 	
+	/**
+	 * This method processes the given FreeMarker template
+	 * with the 'root' map.
+	 * 
+	 * @param templateName The name of the template to be processed.
+	 */
 	private void processTemplate(String templateName){
 		//this method processes the template with the given template name
 		try {
@@ -519,7 +635,12 @@ public class Servlet extends HttpServlet {
 		}
 	}
 	
-	//returns the url for a given request
+	/**
+	 * This method computes and returns the URL for a given request.
+	 * 
+	 * @param request Stores the user's request and parameters.
+	 * @return URL string
+	 */
 	private String getURL(HttpServletRequest request) {
 	    StringBuffer requestURL = request.getRequestURL();
 	    String queryString = request.getQueryString();
@@ -530,7 +651,14 @@ public class Servlet extends HttpServlet {
 	        return requestURL.append('?').append(queryString).toString();
 	    }
 	}
-
+	
+	/**
+	 * This method redirects the user to the last page
+	 * they visited (stored in HttpSession object).
+	 * 
+	 * @param response Stores the user's request and parameters.
+	 * @param session The current user's session and contents.
+	 */
 	private void sendToLastPage(HttpServletResponse response, HttpSession session) {
 		String url = (String) session.getAttribute("url");
 		//send user to previous url if it exists
