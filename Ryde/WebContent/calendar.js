f15891dbc581dfd88f36baa949bd07b4ad75f901
vtec11001 <smn73822@uga.edu>
2016-11-30 01:08:48 -0500

    calendar.js rewrite jQuery
    rewrote calendar.js using date picker UI

--- Ryde/WebContent/calendar.js
+++ Ryde/WebContent/calendar.js
@@ -1,103 +1,92 @@
-function sendInfo()
-{
-    var request;
-    
-    if (window.XMLHttpRequest)
-        {
-                request = new XMLHttpRequest(); 
-        }
-    
-    
-    return request; 
-
-}
+      
+var rentalDates; 
 
 
-
-
-function ajaxCallValidation()
-{
-    var username= document.getElementById("username"); 
-    var password = document.getElementById("password"); 
-    //init username and password. 
-    
-    
-    var request; 
-    
-    if (window.XMLHttpRequest)
-        {
-            request = new XMLHttpRequest(); 
-        }
-    
-    request.open("POST", "Servlet", true); 
-    request.send("username=" + username & "password=" + password);
-    //opens connection to server and sends details. 
-    
-    request.onreadystatechange = funtion()
+ function disableDays(date)
+ {
+    var month = date.getMonth(); 
+    var day = date.getDate();
+    var year = date.getFullYear().toString().substr(2,2); 
+                               
+    var totalDate = month + "/" + day + "/" + year; 
+                                
+                  
+    totalDate = new Date (totalDate); 
+                               
+    console.log(totalDate); 
+    console.log(x1); 
+                  
+    if (totalDate.getTime() == x1.getTime())
     {
-        if (this.status === 200)
-            {
-                document.getElementById("validationFeild").innerHTML = this.resoponseText; 
-            }
+                                       
+        console.log("FOUND IT"); 
+            return [false, "", "not valid"];
     }
-    //when we get a response send the response info back to html. 
-    
-}
-
-
-function ajaxCallCalender()
-{
-    var startDate = document.getElementById("startDate"); 
-    var endDate = document.getElementById("endDate"); 
-    
-    var request; 
-    
-        if (window.XMLHttpRequest)
-        {
-            request = new XMLHttpRequest(); 
-        }
-    
-    request.open("GET", "Servlet", true); 
-    request.send(); 
-    
-    var stringDates; 
-    
-    request.onreadystatechange = function()
-    {
-        if ((this.status === 200) && (this.readyState === 4) && (this.responseText != undefined))
-        {
-            stringDates = JSON.parse(this.responseText); 
-            //when request is done get the response string. 
-            
-            //return this.resoponseText; 
-        }
-        
-        else 
-            stringDates = undefined;   
-    }
-    
-    var jsDates; 
-    
-    if (stringDates != undefined)
-        jsDates = eval(stringDates); 
-    else 
-        jsDates = undefined; 
-    
-    //convert strings to js vars via JSON eval. 
-    
-    return jsDates; 
-    //return json objects. 
-    
-}
-
-function CalenderRender()
-{
-    //assuming 
-    
-    var invalidDates = ajaxCallCalender(); 
-    
-    if (invalidDates != undefined)
-        $('#calendar').availabilityCalendar(invalidDates);
     else
-   
+        return [true]; 
+  }
+      
+
+
+
+
+
+$( function() 
+{
+          //function used to init the calender. 
+          //also blocks out certain dates from the server rented dates. 
+    
+    var dateFormat = "mm/dd/yy",
+        from = $( "#from" )
+            .datepicker({
+              defaultDate: "+1w",
+              changeMonth: true,
+              numberOfMonths: 1, 
+                beforeShowDay: disableDays
+            })
+            .on( "change", function() {
+              to.datepicker( "option", "minDate", getDate(this) );
+            }),
+          to = $( "#to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1, 
+              beforeShowDay: disableDays
+          })
+          .on( "change", function() {
+            from.datepicker( "option", "maxDate", getDate( this ) );
+          });
+
+        function getDate( element ) {
+          //returns the date from the calender. 
+            
+            var date;
+          try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+          } catch( error ) {
+            date = null;
+          }
+
+          return date;
+        }
+      } );
+
+function jqueryCalender()
+{
+    var startDate = document.getElementById("from"); 
+    var endDate = document.getElementById("to"); 
+    
+    
+    $.ajax(url: "Servlet", data: {calenderVal : "calender" }, success:function(data)
+    {
+        data = jQuery.parseJSON(data); 
+        
+        rentalDate = data; 
+        //rental date array set to servlet response. 
+        
+        
+        
+        //$("#calendarArea").availabilityCalendar(data);
+        
+    }); 
 }
