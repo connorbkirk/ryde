078a1106942052e2b75ae4dc3d8b295a2ac86dde
Connor Kirk <ckirk96@gmail.com>
2016-12-01 23:10:32 -0500

    typo

--- Ryde/src/boundary/Searchlet.java
+++ Ryde/src/boundary/Searchlet.java
@@ -1,11 +1,16 @@
+package boundary;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import logiclayer.SearchLogicImpl;
 
 /**
  * Servlet implementation class Searchlet
@@ -28,13 +33,13 @@ public class Searchlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-    SearchLogicImpl sl = new SearchLogicImpl();
-    String currentSearchBarContent = request.getParameter("inputText");
-    List<String> resultList = sl.getSuggestionsFromPersist(currentSearchBarContent);
-    String result = resultList.get(0);
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter;
-    out.println(result);
+	    SearchLogicImpl sl = new SearchLogicImpl();
+	    String currentSearchBarContent = request.getParameter("inputText");
+	    List<String> resultList = sl.getSuggestionsFromPersist(currentSearchBarContent);
+	    String result = resultList.get(0);
+	    response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+	    out.println(result);
 	}
 
 	/**
