cd09fdf413b1b86b0c9655e30423f151e0a17957
Connor Kirk <ckirk96@gmail.com>
2016-12-01 23:08:41 -0500

    typo

--- Ryde/src/logiclayer/SearchLogicImpl.java
+++ Ryde/src/logiclayer/SearchLogicImpl.java
@@ -1,20 +1,10 @@
 //TODO
 package logiclayer;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.sql.Blob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
-
-import javax.servlet.http.Part;
-
-import com.ibm.wsdl.util.IOUtils;
 
 import persistlayer.SearchPersistImpl;
 
@@ -27,28 +17,28 @@ public class SearchLogicImpl{
     }
 
     public List<String> getSuggestionsFromPersist(String currentSearchBarString){
-	ResultSet rs = null;
-	rs = sp.getSuggestions(currentSearchBarString);
-
-	List<String>resultMakes = new ArrayList<String>();
-
-	try 
-	{
-	    while(rs.next())
-	    {
-		String make = rs.getString("make");
-   
-
-		resultMakes.add(make);
-		//dynamically creates strings for makes.                                                                                                                                          
-	    }
-	} catch (SQLException e) {
-	    // TODO Auto-generated catch block                                                                                                                                                  
-	    e.printStackTrace();
+		ResultSet rs = null;
+		rs = sp.getSuggestions(currentSearchBarString);
+	
+		List<String>resultMakes = new ArrayList<String>();
+	
+		try 
+		{
+		    while(rs.next())
+		    {
+			String make = rs.getString("make");
+	   
+	
+			resultMakes.add(make);
+			//dynamically creates strings for makes.                                                                                                                                          
+		    }
+		} catch (SQLException e) {
+		    // TODO Auto-generated catch block                                                                                                                                                  
+		    e.printStackTrace();
+		}
+	
+		return resultMakes;
 	}
-
-	return resultCars;
-    }
 
 
 }
