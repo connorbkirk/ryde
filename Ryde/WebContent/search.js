195af202ba9c9707bffd788cf01678fa656e3c23
Connor Kirk <ckirk96@gmail.com>
2016-12-01 23:25:53 -0500

    fixed typos

--- Ryde/WebContent/search.js
+++ Ryde/WebContent/search.js
@@ -1,24 +1,17 @@
-//This is the js file to implement the car preview feature of the search.
-var searchbar = $("searchbar");
+$(function(){
+	$("#makeSearchbox").keypress(function(){
+		var inputText = $("#makeSearchbox").val();
+		//returns the value of the input area to variable
+		
+		$.ajax({
+			url:"Searchlet",
+			type:"GET",
+			data:"inputText="+inputText,
+			success: function(data){
+				$("#makeSearchbox").val(data);
+			}
+		});
+	})
+});
 
-function showResult(str) {
-  if (str.length==0) { 
-    document.getElementById("jumbotron").innerHTML="";
-    document.getElementById("jumbotron").style.border="0px";
-    return;
-  }
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  } else {  // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("livesearch").innerHTML=this.responseText;
-      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-    }
-  }
-  //replace with ajax //xmlhttp.open("GET","livesearch.php?q="+str,true);
-  //replace with ajax //xmlhttp.send();
-}
+
