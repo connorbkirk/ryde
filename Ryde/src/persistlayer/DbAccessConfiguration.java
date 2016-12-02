56e3e1903843b13dd77a4598e5429cd9753c1889
Connor Kirk <ckirk96@gmail.com>
2016-12-01 22:27:45 -0500

    javadoc

--- Ryde/src/persistlayer/DbAccessConfiguration.java
+++ Ryde/src/persistlayer/DbAccessConfiguration.java
@@ -1,9 +1,14 @@
 package persistlayer;
 
-//this class contains information needed to connect to the database
+/**
+ * This class contains information needed to connect to the database.
+ * 
+ * @author Connor Kirk
+ *
+ */
 public abstract class DbAccessConfiguration {
 	static final String DRIVER_NAME = "com.mysql.jdbc.Driver";				//name of jdb driver
 	static final String CONNECT_URL = "jdbc:mysql://localhost:3306/ryde";	//mysql url
 	static final String USER = "root";										//mysql username
-	static final String PASS = "weatherbox";								//mysql password
+	static final String PASS = "root";										//mysql password
 }
