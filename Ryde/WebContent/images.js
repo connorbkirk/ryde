11b509e060658fc91ad66f55e763c499b6d7fc71
Connor Kirk <ckirk96@gmail.com>
2016-12-02 20:58:11 -0500

    Update and rename edit.js to images.js

--- Ryde/WebContent/images.js
+++ Ryde/WebContent/images.js
@@ -1,29 +1,7 @@
 $(document).ready(function(){
-    $('.text-entry').keyup(doCheck).focusout(doCheck);
     $(".deleteImage").click(deleteImage);
 	$("#upload").change(uploadImage);
 });
-
-
-//make sure all the fields are not empty before allowing user to continue
-function doCheck(){
-    var allFilled = true;
-    $('.text-entry').each(function(){
-        if($(this).val() == ''){
-            allFilled = false;
-            return false;
-        }
-    });
-    if(allFilled && $.isNumeric($("input[name=year]").val()) && $.isNumeric($("input[name=price]").val())){
-    	$('input[type=submit]').removeClass("disabled");
-    	$('input[type=submit]').attr('disabled', false);
-    }
-    else{
-    	$('input[type=submit]').addClass("disabled");
-    	$('input[type=submit]').attr('disabled', true);
-    }
-}
-
 
 function deleteImage(){
 	var id = $(this).attr('id');//get id of image to delete
