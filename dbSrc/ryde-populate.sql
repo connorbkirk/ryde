6bb3cae2f64755a9a4bfa6ce9f109ff55182e07a
Connor Kirk <ckirk96@gmail.com>
2016-12-02 19:21:55 -0500

    Update ryde-populate.sql

--- dbSrc/ryde-populate.sql
+++ dbSrc/ryde-populate.sql
@@ -9,40 +9,40 @@ VALUES('1','John','Porter','jayp','66bb2040b1142c33537b6d91723d4eb6','7054444444
 
 #Add cars to the database
 INSERT INTO cars
-VALUES('1','sedan','Toyota','Camry','2016','silver','1','200','nice and reliable car');
+VALUES('1','Sedan','Toyota','Camry','2016','silver','1','200','nice and reliable car');
 
 INSERT INTO cars
-VALUES('2','sedan','Saturn','Ion','2004','silver', '1','75','Car has been in two accidents, completely fixed up and safe');
+VALUES('2','Sedan','Saturn','Ion','2004','silver', '1','75','Car has been in two accidents, completely fixed up and safe');
 
 INSERT INTO cars
-VALUES('3','truck','Ford','BigTruck','2024','chrome','2','200','You have not driven a car so impractical before');
+VALUES('3','Truck','Ford','BigTruck','2024','chrome','2','200','You have not driven a car so impractical before');
 
 INSERT INTO cars
-VALUES('4','sedan','Mercedes','Mersais','2016','blue','5', '1','He will roll up in the Mersais with a check to your work place' );
+VALUES('4','Sedan','Mercedes','Mersais','2016','blue','5', '1','He will roll up in the Mersais with a check to your work place' );
 
 INSERT INTO cars
-VALUES('5','boat', 'Toyota', 'BigRaft', '2004','white','4','20','Before you say there is no such thing just trust us that it exists');
+VALUES('5','Boat', 'Toyota', 'BigRaft', '2004','white','4','20','Before you say there is no such thing just trust us that it exists');
 
 INSERT INTO cars
-VALUES('6','truck','Ford','F650','2018','black','3','100','Such a big truck it could move a house');
+VALUES('6','Truck','Ford','F650','2018','black','3','100','Such a big truck it could move a house');
 
 INSERT INTO cars
-VALUES('7','suv','Volkswagon','Thing','1972','yellow','2','60','Comes with the parts to turn into boat');
+VALUES('7','SUV','Volkswagon','Thing','1972','yellow','2','60','Comes with the parts to turn into boat');
 
 INSERT INTO cars
-VALUES('8','suv','Kia','Sorento','2017','silver','4','100','Can seat 7 people');
+VALUES('8','SUV','Kia','Sorento','2017','silver','4','100','Can seat 7 people');
 
 INSERT INTO cars
-VALUES('9','sedan','Pontiac','Sunbird','1992','white','4','15','Starts most of the time');
+VALUES('9','Sedan','Pontiac','Sunbird','1992','white','4','15','Starts most of the time');
 
 INSERT INTO cars
-VALUES('10','sedan','Honda','Civic','2004','silver','2','30','Very reliable');
+VALUES('10','Sedan','Honda','Civic','2004','silver','2','30','Very reliable');
 
 INSERT INTO cars
-VALUES('11','suv','Honda','Civic','1996','black','1','20','You could not break this car if you tried');
+VALUES('11','SUV','Honda','Civic','1996','black','1','20','You could not break this car if you tried');
 
 INSERT INTO cars
-VALUES('12','sport','Ford','Mustang','2015','red','6','75','What a stang it is');
+VALUES('12','Sport','Ford','Mustang','2015','red','6','75','What a stang it is');
 
 
 #add rental dates for the cars
